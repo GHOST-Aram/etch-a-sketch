4281d17ecfcfd7410fcfeb8f27b73ed6bdac7c88
GHOST-Aram <owinoojala@gmail.com>
2022-11-24 12:07:43 +0300

    Refactor code in displayInitialMethod():- to display 10 * 10
    >         squares by default
    >        -Add default parameter
    >        -refactor grid template colums to calculates number of
    >         colums using total number of squares

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const container = document.querySelector('.container')
 
 //Display a 16 * 16 grid in the container div 
-function displayIntialGrid(){
-    const grid = 16*16
-    container.style.gridTemplateColumns = 'repeat(16,1fr)'
+function displayIntialGrid(squares=100){
+    const grid = squares
+    container.style.gridTemplateColumns = `repeat(${Math.floor(Math.sqrt(squares))},1fr)`
     for(let index = 0; index < grid; index++){
         const div = document.createElement('div')
         div.classList.add('card')
