157bf2e6a833334adaeb4fdac2ac7b8956814f64
GHOST-Aram <owinoojala@gmail.com>
2022-11-24 12:50:24 +0300

    Add event listener to button(click event)
         -Create method prompUSer():-take imput from user
         -Request input for values outside of 0 and 100

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const container = document.querySelector('.container')
 const section = document.querySelector('section')
 const btn = document.createElement('button')
+// const gridDiv = document.createElement('button')
+
 
 //Create button for initiating user input
 const btnDiv = document.createElement('div')
@@ -11,7 +13,8 @@ btnDiv.appendChild(btn)
 section.insertBefore(btnDiv, container)
 
 //Display a 16 * 16 grid in the container div 
-function displayIntialGrid(squares=100){
+function displayGrid(squares=100){
+   
     const grid = squares
     container.style.gridTemplateColumns = `repeat(${Math.floor(Math.sqrt(squares))},1fr)`
     for(let index = 0; index < grid; index++){
@@ -21,4 +24,19 @@ function displayIntialGrid(squares=100){
     }
 }
 
-displayIntialGrid(squares=80)
+function promptUser(){
+    const squares = parseInt(prompt(`Enter number of squares
+    you would like to display`))
+    while(squares > 100 && squares <=0){
+        const squares = parseInt(prompt(`Enter a maximum of 100`))
+    }
+    return squares
+}
+displayGrid(squares=80)
+
+
+btn.addEventListener('click', (e)=>{
+    const squares = promptUser()
+    displayGrid(squares)
+    console.log(e)
+})
