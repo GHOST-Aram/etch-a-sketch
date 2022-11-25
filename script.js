9ee4f458b6a9d3ba7fb6d984313f4eab81628b2e
GHOST-Aram <owinoojala@gmail.com>
2022-11-25 09:46:33 +0300

    Refactor display algorithm
        -Edit displayGrid() method to display number of squares per side
         equal to direct input value
        -remove draw() method

--- script.js
+++ script.js
@@ -2,25 +2,19 @@ const container = document.querySelector('.container')
 const section = document.querySelector('section')
 
 
-//Creat separate div for the grid
-const gridDiv = document.createElement('button')
-gridDiv.className = 'grid m-auto grid-div'
-container.appendChild(gridDiv)
-
-//Create button for initiating user input
-
-
-
-
 //Display grid in the grid div 
-function displayGrid(squares=100){
-    container.removeChild(btnDiv)
+function displayGrid(squares=16){
+    // container.removeChild(btnDiv)
     gridDiv.innerHTML = ''
-    const grid = squares ** 2
+    const grid = squares ** 2//total number of squares
+    const space = 500 - squares//remaining space for dispalying grid(less grid gap)
     gridDiv.style.gridTemplateColumns = `repeat(${squares},1fr)`
     for(let index = 0; index < grid; index++){
         const div = document.createElement('div')
         div.classList.add('card')
+        div.style.height = `${space/squares}px`
+        div.style.width = `${space/squares}px`
+
         gridDiv.appendChild(div)
 
         div.addEventListener('mouseover', ()=>{
@@ -28,29 +22,18 @@ function displayGrid(squares=100){
         })
     }
 }
-function draw(event){
-    let coordX = event.clientX
-    let coordY = event.clientY
-    ctx.beginPath()
-    ctx.lineWidth = 5
-    ctx.lineCap = 'round'
-    ctx.strokeStyle = '#ACD3ED'
-    ctx.moveTo(coordX,coordY)
-    ctx.lineTo(coordX, coordY)
-    ctx.stroke()
-}
+
 function getRandomValue(min, max){
     let randInt = Math.floor(Math.random() * (max - min) + min)
     return randInt
 }
 function promptUser(){
     try{
-        let squares = parseInt(prompt(`Enter number of squares
-        you would like to display`))
+        let squares = parseInt(prompt(`Enter number of squares per side`))
 
         //Keep requesting input if too samll or too large
         while(squares > 100 || squares <=0){
-            squares = parseInt(prompt(`Enter a maximum of 100`))
+            squares = parseInt(prompt(`Enter a number btwn 0 'n' 100`))
         }
         return squares
     } catch(error){
@@ -66,19 +49,28 @@ function setBackground(element){
     element.style.background = `rgb(${red},${green},${blue})`
    
 }
+//Create button for initiating user input
 
-    const heading = document.createElement('h1')
-    heading.className = 'text-centre'
-    const btn = document.createElement('button')
-    const btnDiv = document.createElement('div')
-    btnDiv.className = 'btn-div text-centre m-auto'
-    btn.className = 'btn'
-    btn.textContent = 'Set Grid'
-    btnDiv.appendChild(btn)
-    container.appendChild(btnDiv)
+const heading = document.createElement('h1')
+heading.className = 'text-centre'
+const btn = document.createElement('button')
+const btnDiv = document.createElement('div')
+btnDiv.className = 'btn-div text-centre m-auto'
+btn.className = 'btn'
+btn.textContent = 'Set Grid'
+btnDiv.appendChild(btn)
+container.appendChild(btnDiv)
+
+//Creat separate div for the grid
+const gridDiv = document.createElement('button')
+gridDiv.className = 'grid m-auto grid-div'
+gridDiv.appendChild(heading)
+container.appendChild(gridDiv)
 
 
 
+
+displayGrid()//Inital grid
 btn.addEventListener('click', (e)=>{
     const squares = promptUser()
     if(!squares){
@@ -87,4 +79,3 @@ btn.addEventListener('click', (e)=>{
     }
     displayGrid(squares)
 })
-gridDiv.addEventListener('mousemove', draw)
